18c2e5b74c01d6a31314919a1fc49c16b254dde3
Abdulhaleem Nasredeen <106285152+nasredeenabdulhaleem@users.noreply.github.com>
2022-05-31 21:48:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-load_dotenv()
 # Enable debug mode.
 DEBUG = True
 
@@ -13,6 +10,4 @@ DEBUG = True
 
 # TODO IMPLEMENT DATABASE URL
 
-
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
-    os.getenv('DB_HOST'), os.getenv('DB_USER'), os.getenv('DB_PASSWORD'), os.getenv('DB_NAME'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:nasredeen@localhost:5432/fyyur'
